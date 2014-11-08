7986f2ecbdf5b08345a1d54959f7cca2dfee20dc
Brian Hiatt <bchiatt>
2014-11-07 23:14:06 -0600

    initial setup

---
+++ www/js/app.js
@@ -0,0 +1,67 @@
+angular.module('gp-nashvesTN', ['ionic', 'gp-nashvesTN.controllers'])
+
+.run(function($ionicPlatform){
+  'use strict';
+
+  $ionicPlatform.ready(function(){
+    if(window.cordova && window.cordova.plugins.Keyboard){
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar){
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider){
+  'use strict';
+
+  $stateProvider
+
+    .state('app', {
+      url: '/app',
+      abstract: true,
+      templateUrl: 'templates/menu.html',
+      controller: 'AppCtrl'
+    })
+
+    .state('app.search', {
+      url: '/search',
+      views: {
+        'menuContent' :{
+          templateUrl: 'templates/search.html'
+        }
+      }
+    })
+
+    .state('app.browse', {
+      url: '/browse',
+      views: {
+        'menuContent' :{
+          templateUrl: 'templates/browse.html'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent' :{
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+    .state('app.single', {
+      url: '/playlists/:playlistId',
+      views: {
+        'menuContent' :{
+          templateUrl: 'templates/playlist.html',
+          controller: 'PlaylistCtrl'
+        }
+      }
+    });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
+});
